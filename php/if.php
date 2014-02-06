9b16c66902850adbe1bfb7b5b6f90e059b5a0909
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-06 15:00:54 -0600

    edit including else and else if
    else/else if

--- php/if.php
+++ php/if.php
@@ -1,33 +1,29 @@
 <?php
-$a = 5;
-$b = 10;
-$c = '10';
+$a = 2;
+$b = 5;
+$c = '5';
 if ($a < $b) {
-	
 	echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
-	
+} else if( $b > $a ) {
 	echo "$b is greater than $a\n";
 }
 
-if ($b > $c) {
-	echo "$b is greater than $c\n";
+if ($b >= $c) {
+	echo "$b is greater than or equal to $c\n";
 }
-if ($b <= $c) {
+else if($b <= $c) {
 	echo "$b is greater than or equal to $c\n";
 }
 if ($b == $c) {
 	echo "$b is equal value $c\n";
 }
-if ($b === $c) {
+elseif ($b === $c) {
 	echo "$b is equal value and type $c\n";
 }
-if ($b != $c) {
+elseif ($b != $c) {
 	echo "$b is not equal to $c\n";
 }
-if ($b !== $c) {
+else if($b !== $c) {
 	echo "$b is not identical to $c\n";
 }
 
