2a1ce174fb33078d9e65959bba312854cbbced36
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-06 14:06:57 -0600

    Control Structures 2
    Logical

---
+++ php/if_logical.php
@@ -0,0 +1,34 @@
+<?php
+$x = 0;
+$y = 5;
+$z = 10;
+
+echo "{$x} < {$y} < {$z}\n";
+
+if (($x > 0) || ($x < 10)) {
+	echo "$x is greater than 0 or less than 10\n";
+
+}
+
+if (($y > 0) || ($y < 10)) {
+	echo "$y is greater than 0 or less than 10\n";
+}
+
+if (($z > 0) || ($z < 10)) {
+	echo "$z is greater than 0 or less than 10\n";
+}
+
+if (($x > 0) && ($x < 10)) {
+	echo "$x is greater than 0 and less than 10\n";
+}
+
+if (($y > 0) && ($y < 10)) {
+	echo "$y is greater than 0 and less than 10\n";
+}
+
+if (($z > 0) && ($z < 10)) {
+	echo "$z is greater than 0 and less than 10\n";
+}
+
+
+?>
