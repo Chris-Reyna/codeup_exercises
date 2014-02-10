afbf46e19aafd3d8e670174c88f7119537874272
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-10 11:27:25 -0600

    foreach last update
    added scalar

--- php/foreach.php
+++ php/foreach.php
@@ -4,17 +4,11 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $thingy) {
 
-	if (is_int($thingy)) {
+	if (is_scalar($thingy)) {
 
-		echo "{$thingy} is an integer\n";	}
+		echo "{$thingy} is a scalar\n";	}
 
-	elseif (is_float($thingy)) {
 
-		echo "{$thingy} is a float\n";	}
-
-	elseif (is_bool($thingy)) {
-
-		echo "{$thingy} is a boolean\n";	}
 
 	elseif (is_array($thingy)) {
 
@@ -23,18 +17,15 @@ foreach ($things as $thingy) {
 			foreach ($thingy as $array) {
 
 				echo "{$array} is a number\n";
-			}	}
+			}
+				}
 
 	elseif (is_null($thingy)){
 
 		echo "{$thingy} is null\n";	}
 
-	elseif (is_string($thingy)) {
 
-		echo "{thingy} is a string\n";
-	}
 }
 
 ?>
 
-}
