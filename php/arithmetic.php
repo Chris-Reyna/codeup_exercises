c57fbf4f7c899447b000ccbcec134f258c49d56c
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-11 11:07:22 -0600

    test functions
    add, sub, multi, div.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,1 +1,33 @@
 <?php
+
+function add($a,$b) {
+
+	echo $a + $b . "\n";
+
+}
+
+function subtract($a,$b) {
+
+	echo $a - $b . "\n";
+
+}
+
+function multiply($a,$b) {
+
+	echo $a * $b . "\n";
+
+}
+
+function divide($a,$b) {
+
+	echo $a / $b . "\n";
+
+}
+
+add(25, 5);
+subtract(40, 10);
+multiply(5,6);
+divide(150, 5);
+
+
+?>
