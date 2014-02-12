f6d7b20eed11b0b62271b959c3ff0580c86739dc
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-12 11:53:42 -0600

    function entered
    entered working function; test for $nothing

--- php/internal-functions.php
+++ php/internal-functions.php
@@ -5,20 +5,52 @@ $something = '';
 $array = array(1,2,3);
 
 // Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
-function is_empty($something) {
 
-	if (isset($something)) {
+function empty_set($check) {
 
-		return "$something is SET|EMPTY";
+	if (isset($check)) {
+
+		return true;
+	}elseif (empty($check)) {	 
+		return true;
+	}else{
+		return false;
 	}
 
+
+}	
+
+
+
+// var_dump(empty_set($something));
+
+
+
+if (empty_set($nothing)) {
+
+		echo "\$nothing is SET\n";
+}else{
+
+	 echo "\$nothing is not SET\n";
 }
 
-var_dump(isset($something));
 
-echo is_empty($something);
+if (empty_set($nothing)) {
+
+		echo "$nothing is empty\n";
+}else{
+		echo "$nothing is not empty\n";
+}
+
+
+//var_dump(isset($nothing));
+//var_dump(empty($nothing));
+
+
+//var_dump(isset($nothing));
+
 // TEST: If var $nothing is set, display '$nothing is SET'
-
+//var_dump(empty_set($nothing));
 // TEST: If var $nothing is empty, display '$nothing is EMPTY'
 
 // TEST: If var $something is set, display '$something is SET'
