148e370c5d1a3763c67b1eb388c1003924d7029a
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-13 11:08:23 -0600

    compare incomplete
    added compare function incomplete

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -7,22 +7,40 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 function search($name, $arrays) {
 
-//	$name = '';
-
-	//$name = 'Bob';
-
+	//set variable to = php search function to new $variables
 	$results = array_search($name, $arrays);
 
-	
-
+	//set results = to false if false or not entered
 	if ($results === FALSE) {
 
+	// if search reads false then return false
 		return FALSE;
 	}
-
+		//if not return true
 	return TRUE;
 }
 
 //var_dump my function and set the name desired and variable to search
 var_dump(search('Tina', $names));
 var_dump(search('Bob', $names));
+
+
+function compare($array1, $array2) {
+
+	$in_common = 0;
+
+	foreach ($array as $name) { //undefiende variable error
+
+		$results = array_search($name, $array2); 
+
+		if (is_numeric($result)) {
+
+			$in_common++;
+		} 
+		return $in_common;
+
+	}
+
+}
+
+echo compare($compare, $names);
