7417bce61cfc7380664342bf30cb37e78a362c4b
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-13 15:48:18 -0600

    sort array alpha
    added sort to function, putting items in alpha

--- php/lists_with_commas.php
+++ php/lists_with_commas.php
@@ -1,33 +1,26 @@
 <?php
 
-//$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+ function humanized_list($string) {
+
+ 	$physicists_array = explode(', ', $string);
+
+ 	sort($physicists_array);
+
+	$last_item = array_pop($physicists_array);
+
+	array_push($physicists_array, 'and ' . $last_item);
+
+	$famous_fake_physicists = implode(', ', $physicists_array);
+
+	return $famous_fake_physicists;
+}
+
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+$famous_fake_physicists = humanized_list($physicists_string);
+
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
 
 
-$physicists_array = array("Gordon Freeman", "Smantha Carter", "Sheldon Cooper", "Quinn Mallory", "Bruce Banner", "Tony Stark");
-//$item = "and";
-$item2 = array_pop($physicists_array);
-array_push($physicists_array, 'and ' . $item2);
-
-
-
- $famous_fake_physicists = implode(', ', $physicists_array);
-
-
- echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
-
-
-//Converts array into list n1, n2, ..., and n3
-// function humanized_list($array) {
-//   // Your solution goes here!
-// }
-
-// // List of famous peeps
-// $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-
-// // Humanize that list
-// $famous_fake_physicists = humanized_list($physicists_string);
-
-// // Output sentence
-// echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
 
 ?>
