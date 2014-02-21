910ee7610bab4be31573906dc63e009bbb523c3a
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-21 14:39:51 -0600

    complete
    added and completed compare functions

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -7,20 +7,14 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 function search($name, $arrays) {
 
-	//set variable to = php search function to new $variables
 	$results = array_search($name, $arrays);
 
-	//set results = to false if false or not entered
 	if ($results === FALSE) {
 
-	// if search reads false then return false
 		return FALSE;
 	}
-		//if not return true
 	return TRUE;
 }
-
-//var_dump my function and set the name desired and variable to search
 var_dump(search('Tina', $names));
 var_dump(search('Bob', $names));
 
@@ -29,18 +23,18 @@ function compare($array1, $array2) {
 
 	$in_common = 0;
 
-	foreach ($array as $name) { //undefiende variable error
+	foreach ($array1 as $key => $value) { //undefiend variable error
 
-		$results = array_search($name, $array2); 
+		$results = array_search($value, $array2); 
 
-		if (is_numeric($result)) {
+		if (is_numeric($results)) {
 
 			$in_common++;
 		} 
-		return $in_common;
 
 	}
+		return $in_common;
 
 }
 
-echo compare($compare, $names);
+echo 'There are ' . compare($names, $compare) . ' names in common' . "\n";
