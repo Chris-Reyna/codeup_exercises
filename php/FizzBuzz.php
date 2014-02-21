71f52a376bc11e90d7f9dbc4eedc1de61d10dde3
Chris-Reyna <christopher.reyna@yahoo.com>
2014-02-21 14:06:00 -0600

    completed
    ;)

--- php/FizzBuzz.php
+++ php/FizzBuzz.php
@@ -1,9 +1,21 @@
 <?php
 
 
-for($i = 1; $i <= 100; 4i==) {
+for($i = 1; $i <= 100; $i++) {
 
-	$fizz =
+	$fizz = ($i % 3 == 0);
 
-	$buzz =
+	$buzz = ($i % 5 == 0);
+
+	$fizzbuzz = ($i % 3 == 0 && $i % 5 == 0);
+	
+	if ($fizzbuzz) {
+		echo "FizzBuzz\n";
+	}elseif ($buzz) {
+		echo "Buzz\n";
+	}elseif ($fizz) {
+		echo "Fizz\n";
+	}else {
+		echo "$i\n";
+	}
 }
